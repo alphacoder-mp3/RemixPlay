b787cd473ee35c69449a3ce001db7f6d76313c26
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-09-26 20:56:59 +0530

    Add app.css file and Update root file

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Form,
   Links,
   Meta,
   Outlet,
@@ -8,6 +9,8 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+
+import appStylesHref from "./app.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,6 +23,7 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: appStylesHref },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -33,6 +37,38 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div
+                aria-hidden
+                hidden={true}
+                id="search-spinner"
+              />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href={`/contacts/1`}>Your Name</a>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
